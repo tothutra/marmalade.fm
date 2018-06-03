7a1d3ca6b5fd5775004624cc11ff170a766aa502
tothutra <tothutra@gmail.com>
2018-06-03 19:00:09 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import FeaturedMix from './FeaturedMix'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          {/*FeaturedMix*/}
+          <FeaturedMix />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeaturedMix.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const FeaturedMix = props => (
+  <div>
+    <p>Featured mix</p>
+    <h1>Mint Condition w/ Hotthobo - 27th November 2017</h1>
+    {/*Play button*/}
+  </div>
+)
+
+export default FeaturedMix
