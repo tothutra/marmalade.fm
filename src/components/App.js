5c05e71e0564aad1457243064c5668593135506f
tothutra <tothutra@gmail.com>
2018-06-03 19:53:42 -0400

    add font and change page title

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FeaturedMix from './FeaturedMix'
+import Header from './Header'
 
 class App extends Component {
   render() {
@@ -8,7 +9,17 @@ class App extends Component {
         <div>
           {/*FeaturedMix*/}
           <FeaturedMix />
+          <div>
+            <Header />
+          </div>
         </div>
+        {/*<AudioPlayer />*/}
+        <iframe
+          width="100%"
+          height="60"
+          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Ffloating-points-jamie-xx-18th-august-2016%2F"
+          frameBorder="0">
+        </iframe>
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Header = props => (
+  <header>
+    <h1></h1>
+    <ul>
+      <li>What’s hot</li>
+      <li>Archive</li>
+      <li>About</li>
+    </ul>
+  </header>
+)
+
+export default Header
