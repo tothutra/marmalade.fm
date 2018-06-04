17e5ef4e701a695c67e6458d5f03b6cf094e5df1
tothutra <tothutra@gmail.com>
2018-06-04 17:01:53 -0400

    add Mix component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Mix from "./Mix"
 
 const Home = props => (
   <div className="flex flex-wrap justify-between mixes ph3 ph4-l">
     <div className="mix mb4">
-      A Mix
+      <Mix
+        name="Ambient treasures"
+        id="/salvatore-muscat/ambient-treasures-vol1-towards-the-dream/"
+      />
     </div>
   </div>
 )
-
 
 export default Home
---
+++ src/components/Mix.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Mix = props => (
+  <div className="aspect-ratio aspect-ratio--3x4 pointer bg-black">
+    <div className="ph3 pv4 aspect-ratio--object mix-overlay">
+      <div className="flex items-center relative z-2">
+        <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{props.name}</h1>
+      </div>
+    </div>
+  </div>
+)
+
+export default Mix
