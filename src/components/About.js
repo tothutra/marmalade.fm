f00a0dd2d2048c7525021c2164f9f87002847263
tothutra <tothutra@gmail.com>
2018-06-08 21:51:24 -0400

    animmated counter with CountUp

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import Counter from './Counter'
 
-const Stat = ({stat, statName, statWord}) => <div className="w-third tc pa3 ba bw2 b--light-gray">
-  <div className="f6  biryani ttu">{statName}</div>
-  <div className="f5 b biryani-black ttu tracked">{stat} {statWord}</div>
+const Stat = ({stat, statName, statWord}) => (<div className="mb4">
+  <div className="f5 black mb0 b">{statName}</div>
+  <Counter end={stat} duration={2.75}/>
+  <div className="f4 lh-1">{statWord}</div>
 </div>
+)
 
 const About = ({mixes, ...props}) => (
-  <div className="ph3 ph4-l">
-    <div className="measue center lh-copy f4 ph3">
-      <p>Marmalade.fm features the latest and greatest in grooves, beats and world music.</p>
+  <div className="ph3 ph4-l measue center">
+    <div className="lh-copy ph3">
+      <p className="mt0">Marmalade.fm features the latest and greatest in grooves, beats and world music.</p>
       <p>Whether you’re into hip hop, trip hop, classic jazz, fusion jazz, afro beat or break beat… we have you covered!</p>
     </div>
-    <div className="flex pt3">
+    <div className="pt3">
       <Stat statName="Featuring..." stat={mixes.length} statWord="mixes"/>
       <Stat statName="Played..." stat={mixes.reduce((accum, current) => accum+current.play_count, 0)} statWord="times"/>
       <Stat statName="With..." stat={mixes.reduce((accum, current) => accum+current.audio_length, 0)} statWord="seconds"/>
